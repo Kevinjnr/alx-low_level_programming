90c9789914150fbf9fac31d9171d99bbb26e0f5e
Kevinjnr <vikzilla.28@gmail.com>
2023-06-19 22:06:01 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int y;
+
+	char g;
+
+	g = 'a';
+	y = 0;
+	while
+		(y < 10) {
+			putchar(y + '0');
+			y++;
+		}
+	while
+		(g <= 'f') {
+			putchar(g);
+			g++;
+		}
+	putchar('\n');
+	return (0);
+}
