ff0df63221012eb0c5c90f15709cd9e591954930
Kevinjnr <vikzilla.28@gmail.com>
2023-06-24 00:10:17 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,20 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int y = 0;
+	int y;
 
-	for (; y <= 9; y++)
+	for (y = '0'; y <= '9'; y++)
 	{
-		if (y = 2 || y == 4)
-	{
-	continue;
-        }
-		else
+		if ((y == '2' || y == '4'))
+			_putchar(y)
 	}
-	else
-	{
-	_putchar(x + '0');
-	}
-        }
+
 	_putchar('\n');
 }
