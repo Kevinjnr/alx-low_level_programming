caf49b08a6a7bd51a2f99a94464d5336a842bc30
Kevinjnr <vikzilla.28@gmail.com>
2023-07-12 13:55:21 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,9 +8,9 @@
  * Return: 0 (success)
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	(void) argv; /*ignore argv*/
+	(void) argv;
 	printf("%d\n", argc - 1);
 
 	return (0);
